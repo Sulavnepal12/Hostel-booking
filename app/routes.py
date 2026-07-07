ef193de26ed61d035f46ffedd0f3cf218516cb3e
Sulav Nepal <your-github-sulavnepal97@gmail.com>
2026-07-07 08:18:38 +0545

    feat: register DELETE routes for hostels and rooms

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint
-from app.controllers.hostel_controller import create_hostel, get_all_hostels, get_hostel_by_id, update_hostel
-from app.controllers.room_controller import create_room, get_all_rooms, get_room_by_id, update_room
+from app.controllers.hostel_controller import create_hostel, get_all_hostels, get_hostel_by_id, update_hostel, delete_hostel
+from app.controllers.room_controller import create_room, get_all_rooms, get_room_by_id, update_room, delete_room
 
 main_bp = Blueprint("main", __name__)
 
@@ -8,8 +8,10 @@ main_bp.route("/hostels", methods=["POST"])(create_hostel)
 main_bp.route("/hostels", methods=["GET"])(get_all_hostels)
 main_bp.route("/hostels/<int:hostel_id>", methods=["GET"])(get_hostel_by_id)
 main_bp.route("/hostels/<int:hostel_id>", methods=["PUT"])(update_hostel)
+main_bp.route("/hostels/<int:hostel_id>", methods=["DELETE"])(delete_hostel)
 
 main_bp.route("/rooms", methods=["POST"])(create_room)
 main_bp.route("/rooms", methods=["GET"])(get_all_rooms)
 main_bp.route("/rooms/<int:room_id>", methods=["GET"])(get_room_by_id)
 main_bp.route("/rooms/<int:room_id>", methods=["PUT"])(update_room)
+main_bp.route("/rooms/<int:room_id>", methods=["DELETE"])(delete_room)
