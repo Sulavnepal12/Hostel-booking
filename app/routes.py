4b7c8e58f2f9b797df623ac2f7a8ce963f825029
Sulav Nepal <your-github-sulavnepal97@gmail.com>
2026-07-08 17:10:19 +0545

    feat: register booking status and cancel routes

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
 from flask import Blueprint
 from app.controllers.hostel_controller import create_hostel, get_all_hostels, get_hostel_by_id, update_hostel, delete_hostel
 from app.controllers.room_controller import create_room, get_all_rooms, get_room_by_id, update_room, delete_room
-from app.controllers.booking_controller import create_booking, get_all_bookings, get_booking_by_id
+from app.controllers.booking_controller import create_booking, get_all_bookings, get_booking_by_id, update_booking_status, cancel_booking
 
 main_bp = Blueprint("main", __name__)
 
@@ -20,3 +20,5 @@ main_bp.route("/rooms/<int:room_id>", methods=["DELETE"])(delete_room)
 main_bp.route("/bookings", methods=["POST"])(create_booking)
 main_bp.route("/bookings", methods=["GET"])(get_all_bookings)
 main_bp.route("/bookings/<int:booking_id>", methods=["GET"])(get_booking_by_id)
+main_bp.route("/bookings/<int:booking_id>/status", methods=["PUT"])(update_booking_status)
+main_bp.route("/bookings/<int:booking_id>/cancel", methods=["PUT"])(cancel_booking)
