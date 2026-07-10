71aa4bbb841d836121f70117964cc0c78cfb6a69
Sulav Nepal <your-github-sulavnepal97@gmail.com>
2026-07-10 15:24:27 +0545

    feat: add warden contact fields to Hostel model

--- app/models/hostel.py
+++ app/models/hostel.py
@@ -2,14 +2,15 @@ from app.extension import db
 
 class Hostel(db.Model):
     __tablename__ = "hostels"
-
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     location = db.Column(db.String(150), nullable=False)
     price_per_bed = db.Column(db.Float, nullable=False)
     amenities = db.Column(db.String(300))
     total_rooms = db.Column(db.Integer, default=0)
-
+    warden_name = db.Column(db.String(100))
+    warden_phone = db.Column(db.String(20))
+    warden_email = db.Column(db.String(100))
     rooms = db.relationship("Room", backref="hostel", lazy=True)
 
     def __repr__(self):
